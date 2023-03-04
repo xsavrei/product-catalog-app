d15d4d64b4f579ac33cf2184e736fa456ef35382
isavrei <xsavrei@gmail.com>
2023-03-04 20:40:36 +0100

    ng analytics

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -6,71 +6,71 @@ import { UntilDestroy } from '@ngneat/until-destroy';
 
 @UntilDestroy()
 @Component({
-  selector: 'app-products-list',
-  templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.scss']
+    selector: 'app-products-list',
+    templateUrl: './products-list.component.html',
+    styleUrls: ['./products-list.component.scss']
 })
 export class ProductsListComponent implements OnChanges {
 
-  @Input()
-  categoryId?: string;
+    @Input()
+    categoryId?: string;
 
-  @Input()
-  sortingOption?: SortingOption;
+    @Input()
+    sortingOption?: SortingOption;
 
-  products?: Product[];
-  copyProducts?: Product[];
+    products?: Product[];
+    copyProducts?: Product[];
 
-  constructor(private store: Store<PartialRootState>) {
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.categoryId?.currentValue) {
-      this.store.select(selectProductsByCategoryId(this.categoryId)).subscribe(res => {
-        this.products = res;
-        this.copyProducts = res;
-      });
-    } else if (changes.categoryId && !this.categoryId) {
-      this.store.select(selectAllProducts).subscribe(res => {
-        this.products = res;
-        this.copyProducts = res;
-      });
+    constructor(private store: Store<PartialRootState>) {
     }
-    if (this.products) {
-      if (changes.sortingOption?.currentValue) {
-        const copyToSort: Product[] = JSON.parse(JSON.stringify(this.products));
-        if (this.sortingOption === SortingOption.NAME) {
-          this.products = copyToSort.sort((a, b) => (a.name ?? '').localeCompare(b.name ?? ''));
-        }
-        if (this.sortingOption === SortingOption.PRICE) {
-          this.products = copyToSort.sort((a, b) => (a.price ?? 0) - (b.price ?? 0));
-        }
-      } else if (!changes.sortingOption?.currentValue && !this.sortingOption) {
-        this.products = this.copyProducts;
 
-      }
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes.categoryId?.currentValue) {
+            this.store.select(selectProductsByCategoryId(this.categoryId)).subscribe(res => {
+                this.products = res;
+                this.copyProducts = res;
+            });
+        } else if (changes.categoryId && !this.categoryId) {
+            this.store.select(selectAllProducts).subscribe(res => {
+                this.products = res;
+                this.copyProducts = res;
+            });
+        }
+        if (this.products) {
+            if (changes.sortingOption?.currentValue) {
+                const copyToSort: Product[] = JSON.parse(JSON.stringify(this.products));
+                if (this.sortingOption === SortingOption.NAME) {
+                    this.products = copyToSort.sort((a, b) => (a.name ?? '').localeCompare(b.name ?? ''));
+                }
+                if (this.sortingOption === SortingOption.PRICE) {
+                    this.products = copyToSort.sort((a, b) => (a.price ?? 0) - (b.price ?? 0));
+                }
+            } else if (!changes.sortingOption?.currentValue && !this.sortingOption) {
+                this.products = this.copyProducts;
+
+            }
+        }
     }
-  }
 
-  /* FIXME problem s realizaciou
-   findAllNestedCategoriesIds(categoryId: string, categoryTree: CategoryNode[]): string | undefined {
-      for (let category of categoryTree) {
-        if (category.id === categoryId) {
-          if (category.children && category.children.length > 0) {
-            for (let children of category.children) {
-              if (children.id && children.children) {
-                this.findAllNestedCategoriesIds(children.id, category.children)
+    /* FIXME problem s realizaciou
+     findAllNestedCategoriesIds(categoryId: string, categoryTree: CategoryNode[]): string | undefined {
+        for (let category of categoryTree) {
+          if (category.id === categoryId) {
+            if (category.children && category.children.length > 0) {
+              for (let children of category.children) {
+                if (children.id && children.children) {
+                  this.findAllNestedCategoriesIds(children.id, category.children)
+                }
               }
             }
           }
         }
+        return ''
       }
-      return ''
-    }
 
-    filterProductsByCategoryId(categoryId: string, products: Product[]): Product[] {
-      return products.filter(product => product.categoryId === categoryId);
-    }*/
+      filterProductsByCategoryId(categoryId: string, products: Product[]): Product[] {
+        return products.filter(product => product.categoryId === categoryId);
+      }*/
 }
 
 
