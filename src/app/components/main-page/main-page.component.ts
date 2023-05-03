d12a70fd690a35474bc9783aa7ecb6e7cb65f06c
Illia Savrei <xsavrei@gmail.com>
2023-05-03 11:23:41 +0200

    cleanup1

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -21,6 +21,7 @@ export class MainPageComponent {
   categories?: Category[];
   activeCategory?: Observable<string | undefined>;
   currentCategoryId?: string;
+
   sortingOption?: SortingOption;
   filterValue?: string;
 
